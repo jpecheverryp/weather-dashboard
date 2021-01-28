4c2cd77ac5fa6018553f610196d00aaff74e5a14
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-01-28 15:42:13 -0500

    cities now appear on the left when they are searched

--- assets/main.js
+++ assets/main.js
@@ -5,30 +5,47 @@ $().ready(function () {
     // Using PascalCase for the object inside local storage
     // Using camelCase for the object in javascript
     if (localStorage.getItem('WeatherHistory') === null) {
-        var weatherHistory = { cities: ['Atlanta'] };
+        var weatherHistory = { cities: [] };
         localStorage.setItem('WeatherHistory', JSON.stringify(weatherHistory));
     } else {
         weatherHistory = JSON.parse(localStorage.getItem('WeatherHistory'));
     }
 
-    // Target Elements on the screen
+    // Target Elements of Page
     var searchInput = $('#search-input');
     var searchBtn = $('#search-btn');
+    var citiesList = $('#cities-list');
 
     // Functions 
+    // This function checks if a city is in saved in the local Storage, then saves it if it's not included
     function insertToLocal(city) {
         if (weatherHistory.cities.includes(city) === false) {
             weatherHistory.cities.push(city);
             localStorage.setItem('WeatherHistory', JSON.stringify(weatherHistory));
-            console.log(weatherHistory);
         }
+    }
+    // This function clears the list of recent searches and 
+    // then displays the buttons for each city that is stored in the local storage 
+    function displayRecentCities() {
+        citiesList.empty();
+        weatherHistory.cities.forEach(element => {
+            var currentCity = $('<div>').text(element);
+            currentCity.addClass(['list-group-item', 'list-group-item-action']);
+            currentCity.attr('data-city', element);
+            citiesList.append(currentCity);
+        });
     }
 
     // Click Events
     searchBtn.on('click', function (e) {
         e.preventDefault();
+        // We insert the city that was searched into the localStorage
         insertToLocal(searchInput.val());
+        // Empties input box to be used again
         searchInput.val('');
+        // Displays the recent searches
+        displayRecentCities();
     });
 
+    displayRecentCities();
 });
